cc401aebc45113acda6b578df60954fc878b844d
Milkriver <mapav4@yandex.ru>
2021-10-29 22:01:55 +0500

    useState

--- project/src/components/movie-card-element/movie-card-element.tsx
+++ project/src/components/movie-card-element/movie-card-element.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-
 type MovieCardElementProps = {
   filmName: string,
   src: string,
+  id: number,
+  onMouseOver: (id: number | undefined) => void
 }
 
-export function MovieCardElement({filmName, src}: MovieCardElementProps): JSX.Element {
+export function MovieCardElement({ filmName, src, id, onMouseOver }: MovieCardElementProps): JSX.Element {
   return (
-    <article className="small-film-card catalog__films-card">
+    <article className="small-film-card catalog__films-card" onMouseEnter={() => onMouseOver(id)} onMouseOut={() => onMouseOver(undefined)}>
       <div className="small-film-card__image">
         <img src={src} alt={filmName} width="280" height="175" />
       </div>
       <h3 className="small-film-card__title">
         <a className="small-film-card__link" href="film-page.html">{filmName}</a>
       </h3>
+      <div>{id}</div>
     </article>
   );
 }
