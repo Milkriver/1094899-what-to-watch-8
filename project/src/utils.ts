55265b4d53992ebe25108c27c4abaef22f571ca6
Milkriver <mapav4@yandex.ru>
2021-11-23 22:23:02 +0500

    Отображение даты в отзыве

--- project/src/utils.ts
+++ project/src/utils.ts
@@ -33,3 +33,51 @@ export const getMovieTime = (time: number): string => {
     `- ${minutesTimer}:${secondsTimer}`;
 };
 
+export const getReviewDate = (date: string): string => {
+  const dateWithoutTime = date.substring(0, 10);
+  const year = dateWithoutTime.slice(0, 4);
+  const day = dateWithoutTime.slice(8, 10);
+  const month = dateWithoutTime.slice(5, 7);
+  let monthName = '';
+  switch (month) {
+    case '01':
+      monthName = 'January';
+      break;
+    case '02':
+      monthName = 'February';
+      break;
+    case '03':
+      monthName = 'March';
+      break;
+    case '04':
+      monthName = 'April';
+      break;
+    case '05':
+      monthName = 'May';
+      break;
+    case '06':
+      monthName = 'June';
+      break;
+    case '07':
+      monthName = 'July';
+      break;
+    case '08':
+      monthName = 'August';
+      break;
+    case '09':
+      monthName = 'September';
+      break;
+    case '10':
+      monthName = 'October';
+      break;
+    case '11':
+      monthName = 'November';
+      break;
+    case '12':
+      monthName = 'December';
+      break;
+  }
+  return `${monthName} ${day}, ${year}`;
+
+};
+
